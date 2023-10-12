caf321c90710a5f13510da5d2843537346b28d53
csm <csm009177@gamil.com>
2023-10-13 08:39:46 +0900

    make two console.log in http.createServer fnc

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const http = require('http');
 const fs = require('fs')
 
 http.createServer(function(request, response){
-
+  console.log(request.method);
+  console.log(request.url);
 })
 
-console.log(typeof(http))
+console.log(meth)
 console.log(typeof(http.createServer))
 console.log(typeof(http.createServer(function(){})))
 console.log(typeof(http.createServer(function(request, response){})))
+
