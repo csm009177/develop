11e27830d3f50e594cd5fed6d321229353f5ce2a
csm <csm009177@gamil.com>
2023-10-13 08:35:13 +0900

    check typeof http, createServer

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const http = require('http');
 const fs = require('fs')
 
+http.createServer(function(request, response){
+
+})
+
+console.log(typeof(http))
+console.log(typeof(http.createServer))
+console.log(typeof(http.createServer(function(){})))
+console.log(typeof(http.createServer(function(request, response){})))
