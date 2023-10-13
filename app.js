eb65ac59273337bf8144d941c2458d290c859978
csm <csm009177@gmail.com>
2023-10-13 09:57:31 +0900

    type listen(8000)

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const fs = require('fs')
 http.createServer(function(request, response){
   console.log(request.method);
   console.log(request.url);
-})
+}).listen(8000);
 
-// console.log(typeof(http))
-// console.log(typeof(http.createServer))
-// console.log(typeof(http.createServer(function(){})))
-// console.log(typeof(http.createServer(function(request, response){})))
+console.log(typeof(http))
+console.log(typeof(http.createServer))
+console.log(typeof(http.createServer(function(){})))
+console.log(typeof(http.createServer(function(request, response){})))
 
